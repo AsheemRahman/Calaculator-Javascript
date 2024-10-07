a67a212aa5d6d81a18b31f18003b4d76f65e4186
Asheem Rahman <mhdasheem@gmail.com>
2024-10-07 11:23:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function buttonclick(val) {
+    document.getElementById("screen").value += val
+}
+
+function clearscreen() {
+    document.getElementById("screen").value = ""
+}
+
+function del() {
+    var input = document.getElementById("screen");
+    input.value = input.value.substring(0, input.value.length - 1)
+}
+
+function equal() {
+    var text = document.getElementById("screen").value
+        var result = eval(text)
+        document.getElementById("screen").value = result
+}
